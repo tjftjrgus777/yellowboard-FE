ad0bb50f2cb6a9105bf969c28fa47f6abf293ceb
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-07-04 09:40:28 +0900

    Feat: 회원가입 페이지 구조 수정

---
+++ src/apis/response/auth/index1.ts
@@ -0,0 +1,9 @@
+import IdCheckResponseDto from "./id-check.response.dto";
+import EmailCertificationResponseDto from "./email-certification.response.dto";
+import CheckcertificationResponseDto from "./check-certification.response.dto";
+
+export type {
+    IdCheckResponseDto,
+    EmailCertificationResponseDto,
+    CheckcertificationResponseDto
+}
